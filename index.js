ad2e68fb1bf656f075603ebe05c7432dc3f7cbb7
Shouvik786 <Cristinoshouvik786@gmail.com>
2024-04-25 15:28:03 +0530

    index js change

---
+++ index.js
@@ -0,0 +1,15 @@
+const Header = document.getElementById("header");
+//header.style.color = 'orange';
+header.style.backgroundColor ="green";
+header.style.borderBottom = "2px solid orange";
+
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World"
+mainHeading.style.color = "orange";
+
+const basketHeading = document.getElementById("basket-heading");
+basketHeading.style.color = "green";
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = "<p>Please visit us again</p>";
+
