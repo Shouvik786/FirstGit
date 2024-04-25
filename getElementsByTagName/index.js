22a598b0c33167e2cf67ef677118baf34b22d7f3
Shouvik786 <Cristinoshouvik786@gmail.com>
2024-04-25 21:10:43 +0530

    getElementsByTagName

---
+++ getElementsByTagName/index.js
@@ -0,0 +1,6 @@
+// Write your code below:
+const listitems = document.getElementsByTagName("li");
+listitems[4].style.color = 'blue';
+for(let i = 0; i < listitems.length; i++){
+ listitems[i].style.fontStyle = 'italic';
+}
